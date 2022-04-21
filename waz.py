2a6e8c0000ef50de966d0e02bf6ec6aeaa7f5ec8
dusikowski <97994107+dusikowski@users.noreply.github.com>
2022-04-21 08:45:44 +0200

    Update waz.py

--- waz.py
+++ waz.py
@@ -1,10 +1,15 @@
 import pygame
+
+
 class Snake():
     #konstruktor klasy
     def __init__(self):
         self.dlugosc=1
         self.punkty=0
         self.pozycje=[(120,120)]
+        self.kierunek=(0,1)
+    def setDirection(self,kier):
+        self.kierunek=kier
     #pobranie pozycji głowy
     def getHead(self):
         return self.pozycje[-1]
@@ -14,9 +19,13 @@ class Snake():
     def drawSnake(self,OknoGry):
          for wspolrzendne in self.pozycje[::-1]: 
             wazShape=pygame.Rect((wspolrzendne[0],wspolrzendne[1]),(40,40))
-            pygame.draw.rect(OknoGry,(255,192,203),wazShape) 
             pygame.draw.rect(OknoGry,(255,192,203),wazShape)
-    def snakeMove(self,x,y):
+    def snakeMove(self):
+        #ostatnia pozycja weza
+        ostatniaPozycja=self.pozycje[-1]
+        #nowe pozycje
+        x=ostatniaPozycja[0]+40*self.kierunek[0]
+        y=ostatniaPozycja[1]+40*self.kierunek[1]
         #sprawdzenie przejścia krawędzi
         noweWspl=self.checkBorder(x,y)
          #sprawdzenie czy wąż sam siebie nie zjadł
@@ -42,4 +51,4 @@ class Snake():
             #przejście góra
         if zmienna2<0:
             zmienna2=400
-        return (zmienna1,zmienna2)    
+        return (zmienna1,zmienna2)  
