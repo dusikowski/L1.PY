71bd087ef3a534aabe7c50f350c6e35a4e71f13e
dusikowski <97994107+dusikowski@users.noreply.github.com>
2022-05-16 12:40:06 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,16 +1,22 @@
 import pygame
 import pygame_menu
-import lekcjia1
+import lekcja1
 def zmianaRozdielczosci(nazwaPola,atrybut):
-    lekcjia1.zmianaRozdielczosci=atrybut
+    lekcja1.zmianaRozdielczosci=atrybut
+def zmienKolorWaz1(Value):
+    lekcja1.zmienaKolorWaz1(Value)
+def zmienKolorWaz2(Value):
+    lekcja1.zmienaKolorWaz2(Value)
 def main():
     pygame.init()
-    OknoMenu=pygame.display.set_mode((400,400))
+    OknoMenu=pygame.display.set_mode((800,800))
     pygame.display.set_caption("Menu Snake")
-    menu = pygame_menu.Menu('Snake 3TI', 400, 400,theme=pygame_menu.themes.THEME_DARK)
-    menu.add.button("start gry",lekcjia1.main,background_color=(0,0,0))
+    menu = pygame_menu.Menu('Snake 3TI',800,800,theme=pygame_menu.themes.THEME_DARK)
+    menu.add.button("start gry",lekcja1.main,background_color=(0,0,0))
     menu.add.selector("rozmiar ekranu",[('400x400',400),('600x600',600),('800x800',800)],onchange=zmianaRozdielczosci)
-    menu.mainloop(OknoMenu)
-    
-main()
+    menu.add.color_input("kolor wąż 1",'rgb',default=(25,25,100),onreturn=zmienKolorWaz1)
+    menu.add.color_input("kolor wąż 2",'rgb',default=(25,25,100),onreturn=zmienKolorWaz2)
 
+    menu.mainloop(OknoMenu)
+
+main()
