daf3771338ae1212dcf8e56c68f40d0f15b35561
dusikowski <97994107+dusikowski@users.noreply.github.com>
2022-05-24 09:47:59 +0200

    Update jb.py

--- jb.py
+++ jb.py
@@ -1,10 +1,11 @@
 import random
 import pygame
 import lekcja1
+
 class Jablko():
     #konstruktor klasy
     def __init__(self):
-        self.kolor=(181,251,111)
+        self.kolor=(193,103,103)
         self.applePosition=(20,20)
         self.randomPosition()
     def setPosition(self,x,y):
@@ -12,9 +13,9 @@ class Jablko():
     def getPosition(self):
         return self.applePosition
     def randomPosition(self):
-        iloscKratek=lekcja1.rozdzielczosc//40-1
-        xApple=random.randint(0,iloscKratek)*40+20
-        yApple=random.randint(0,iloscKratek)*40+20
+        ilosckratek=lekcja1.rozdzielczosc//40-1
+        xApple=random.randint(0,ilosckratek)*40+20
+        yApple=random.randint(0,ilosckratek)*40+20
         self.setPosition(xApple,yApple)
     def drawApple(self,OknoGry):
         pygame.draw.circle(OknoGry,self.kolor,(self.applePosition[0],self.applePosition[1]),20)
